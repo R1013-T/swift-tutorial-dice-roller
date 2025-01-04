f7f4ed01dd2c1eec63722e671b1c1055165ace63
R1013-T <takahashi.ryunosuke.job@gmail.com>
2025-01-04 23:41:25 +0900

    Initial Commit

---
+++ SwiftTutorialDiceRoller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTutorialDiceRoller
+//
+//  Created by 髙橋龍之介 on 2025/01/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTutorialDiceRoller/SwiftTutorialDiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTutorialDiceRollerApp.swift
+//  SwiftTutorialDiceRoller
+//
+//  Created by 髙橋龍之介 on 2025/01/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTutorialDiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
